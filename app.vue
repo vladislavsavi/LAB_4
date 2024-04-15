bb87ac8cfb57b46df71eb77f404af12ec91e78d5
vladislav <vladislavsavi@gmail.com>
2024-04-15 11:38:22 +0500

    init

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+   Hello
+    {{count}}
+    <button @click="count ++">click</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const count = useState(() => 0);
+</script>
